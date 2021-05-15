04bf7ab7f3666ce7681149ca531bb417b71470b0
naomihc <naomihalimun95@gmail.com>
2021-05-15 17:19:35 +0700

    Memperbaiki datatables

--- laravel-donasi-2021/resources/views/layouts/master.blade.php
+++ laravel-donasi-2021/resources/views/layouts/master.blade.php
@@ -4,18 +4,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Donasi 2021</title>
+    <title>DataTable - Mazer Admin Dashboard</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/bootstrap.css">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
 
-    <link rel="stylesheet" href="../assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
-    <link rel="stylesheet" href="../assets/vendors/bootstrap-icons/bootstrap-icons.css">
-    <link rel="stylesheet" href="../assets/css/app.css">
-    <link rel="shortcut icon" href="../assets/images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="assets/vendors/simple-datatables/style.css">
+
+    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
 </head>
 
 <body>
@@ -30,10 +32,15 @@
             @yield('content')
         </div>
     </div>
-    <script src="../assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="../assets/js/bootstrap.bundle.min.js"></script>
-
-    <script src="../assets/js/main.js"></script>
+    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendors/simple-datatables/simple-datatables.js"></script>
+    <script>
+        // Simple Datatable
+        let table1 = document.querySelector("@yield('tableId')");
+        let dataTable = new simpleDatatables.DataTable(table1);
+    </script>
+    <script src="assets/js/main.js"></script>
 </body>
 
 </html>
--- laravel-donasi-2021/resources/views/pages/admin/index.blade.php
+++ laravel-donasi-2021/resources/views/pages/admin/index.blade.php
@@ -1,26 +1,37 @@
 @extends('layouts.master')
+@section('tableId', '#table1')
 @section('content')
     <div class="page-heading">
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
                     <h3>Dashboard Admin</h3>
-                    <p class="text-subtitle text-muted">The default layout </p>
+                    <p class="text-subtitle text-muted"></p>
                 </div>
                 @include('includes.greeting')
             </div>
         </div>
         <section class="section">
+            {{-- Card Manajemen User --}}
             <div class="card">
                 <div class="card-header">
                     <h4 class="card-title">Default Layout</h4>
                 </div>
                 <div class="card-body">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi? Ullam quaerat
-                    similique iusto
-                    temporibus, vero aliquam praesentium, odit deserunt eaque nihil saepe hic deleniti? Placeat
-                    delectus
-                    quibusdam ratione ullam!
+                    <table class="table table-striped" id="table1">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>City</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </section>
